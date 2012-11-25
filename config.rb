f6814c1078dd0da1f053b0360fd48a0cd3b61964
René Zschoch <rene.zschoch@googlemail.com>
2012-11-25 19:28:57 +0100

    changes config.rb to use relative urls

--- config.rb
+++ config.rb
@@ -71,15 +71,14 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
