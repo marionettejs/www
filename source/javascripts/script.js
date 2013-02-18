fb4cd727e2e87c7f35633093df17c796b84ef989
René Zschoch <rene.zschoch@googlemail.com>
2013-02-18 16:05:28 +0100

    add a little surprise

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,7 +1,16 @@
 $(function() {
   $('.header__introduction__body').fitText(1.2, {minFontSize: '16px', maxFontSize: '48px'});
 
-  $("section").fitVids();
+  $('section').fitVids();
+
+  $('.animation-toggle').hover(
+    function () {
+      $('.header__item--logo').toggleClass('animating');
+    },
+    function () {
+      $('.header__item--logo').toggleClass('animating');
+    }
+  );
 
   $('a[href*=#]').each(function() {
     if($(this).attr('href').indexOf('#') === 0) {
