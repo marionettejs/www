bc85dc8cfb5e037f7ed7d2ada40374ac2608f63b
Andrew Humphreys <ahumphreys87@googlemail.com>
2014-05-17 21:18:14 +0100

    add index file to docs

--- source/doc-assets/build.js
+++ source/doc-assets/build.js
@@ -16,6 +16,8 @@ mdoc.run({
 
     templatePath : __dirname + '/custom_template',
 
+    indexContent: '<h1>Backbone.Marionette Documentation.</h1><p>Please use the links in the sidebar or below to find what you are looking for.</p>',
+
     //by default it will look at an `assets_` folder inside the `templatePath`
     assetsPath : __dirname + '/custom_assets',
 
